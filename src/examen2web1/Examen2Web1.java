81d0d5a90332250cbf6a77606cb82a217f5f2cb1
SantiVelez1012 <santivekez1012@gmail.com>
2021-06-24 08:25:57 -0500

    ejercicio 1 y principios del 2

--- src/examen2web1/Examen2Web1.java
+++ src/examen2web1/Examen2Web1.java
@@ -5,17 +5,63 @@
  */
 package examen2web1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author user
  */
 public class Examen2Web1 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
+    
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        
+        // EJERCICIO 1
+        
+        
+        /*
+        double a, b, c, d, e, f, x, y;
+        
+        
+        
+        System.out.println("Ingrese el coeficiente a: ");
+        a = teclado.nextDouble();
+        
+        System.out.println("Ingrese el coeficiente b: ");
+        b = teclado.nextDouble();
+        
+        System.out.println("Ingrese el coeficiente c: ");
+        c = teclado.nextDouble();
+        
+        System.out.println("Ingrese el coeficiente d: ");
+        d = teclado.nextDouble();
+        
+        System.out.println("Ingrese el coeficiente e: ");
+        e = teclado.nextDouble();
+        
+        System.out.println("Ingrese el coeficiente f: ");
+        f = teclado.nextDouble();
+        
+        x = (((c*e) - (b*f))/((a*e) - (b*d)));
+        
+        y = (((a*f) - (c*d))/((a*e) - (b*d)));
+        
+        System.out.println("El valor de x es: " + x);
+        System.out.println("El valor de y es: " + y);
+        
+        */
+        
+        // SEGUNDO EJERCICIO
+        
+        int cantNumeros;
+        System.out.println("Ingrese la cantidad de numeros a digitar: ");
+        cantNumeros = teclado.nextInt();
+        
+        for(int i = 0; i < cantNumeros; i++)
+        
     }
     
 }
